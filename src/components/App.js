2b360c7b71c6dcd2f43952f40a95003c2d7ece67
Asher <Asher.areba@gmail.com>
2023-01-15 10:20:38 +0300

    COmpeleted assignment

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,24 @@
 import React from "react";
 import blogData from "../data/blog";
+import logo from "../assets/logo.svg";
+import Header from "../components/Header";
+import About from "../components/About";
 
 console.log(blogData);
 
 function App() {
   return (
+
     <div className="App">
-        <container>
+      <container>
+
           <App/>
           <App header/>
           <App aside/>
           <App main/>
           
         </container>
+        
     </div>
   );
 }
